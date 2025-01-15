5e372572643133b6a776791c806b0d83cbfea0ba
ericgpinto <ericgrandopinto@gmail.com>
2025-01-15 11:54:40 -0300

    feat: desafio usecases

---
+++ src/usecase/product/find/find.product.unit.spec.ts
@@ -0,0 +1,53 @@
+import { Sequelize } from "sequelize-typescript";
+import Product from "../../../domain/product/entity/product";
+import FindProductUseCase from "./find.product.usecase";
+import FindCustomerUseCase from "../../customer/find/find.customer.usecase";
+
+const product = new Product("123", "Product 1", 125.68);
+
+const MockRepository = () => {
+  return {
+    find: jest.fn().mockReturnValue(Promise.resolve(product)),
+    findAll: jest.fn(),
+    create: jest.fn(),
+    update: jest.fn(),
+  };
+};
+
+describe("Unit Test find customer use case", () => {
+  it("should find a product", async () => {
+    const productRepository = MockRepository();
+    const usecase = new FindProductUseCase(productRepository);
+
+    const input = {
+      id: "123",
+    };
+
+    const output = {
+      id: "123",
+      name: "Product 1",
+      price: 125.68,
+    };
+
+    const result = await usecase.execute(input);
+
+    expect(result).toEqual(output);
+  });
+
+  it("should not find a product", async () => {
+    const productRepository = MockRepository();
+    productRepository.find.mockImplementation(() => {
+      throw new Error("Product not found");
+    });
+
+    const usecase = new FindProductUseCase(productRepository);
+
+    const input = {
+      id: "123",
+    };
+
+    expect(() => {
+      return usecase.execute(input);
+    }).rejects.toThrow("Product not found");
+  });
+});
---
+++ src/usecase/product/update/update.product.unit.spec.ts
@@ -0,0 +1,29 @@
+import Product from "../../../domain/product/entity/product";
+import UpdateProductUseCase from "./update.product.usecase";
+const product = new Product("123", "Product 1", 264.46);
+
+const input = {
+  id: product.id,
+  name: "John Updated",
+  price: product.price,
+};
+
+const MockRepository = () => {
+  return {
+    create: jest.fn(),
+    findAll: jest.fn(),
+    find: jest.fn().mockReturnValue(Promise.resolve(product)),
+    update: jest.fn(),
+  };
+};
+
+describe("Unit test for customer update use case", () => {
+  it("should update a product", async () => {
+    const productRepository = MockRepository();
+    const useCase = new UpdateProductUseCase(productRepository);
+
+    const output = await useCase.execute(input);
+
+    expect(output).toEqual(input);
+  });
+});
